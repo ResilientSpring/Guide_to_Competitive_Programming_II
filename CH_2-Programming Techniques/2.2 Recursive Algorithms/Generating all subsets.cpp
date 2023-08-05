073ce1e2171f6f0616a2caa5a700d3332d6287af
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-05 18:47:43 +0800

    Committed 2023/08/05

--- CH_2-Programming Techniques/2.2 Recursive Algorithms/Generating all subsets.cpp
+++ CH_2-Programming Techniques/2.2 Recursive Algorithms/Generating all subsets.cpp
@@ -4,15 +4,24 @@ using namespace std;
 
 int n = 3;
 
+// A vector that will contain the elements of each subsets.
+vector<int> subset;
+
 int main() {
 
-	// A vector that will contain the elements of each subsets.
-	vector<int> subset;
 
 }
 
 void search(int k) {
 
-	if(k == n + 1)
+	if (k == n + 1) {
+		// process subset
+	}
+	else {
+		// include k in the subset.
+		subset.p
+	}
+		
+
 
 }
